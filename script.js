2c0b14dc9c83fcbdfe5fc725721346e0a6efb4b1
edieemm <47989172+edieemm@users.noreply.github.com>
2019-04-08 18:34:37 +0300

    js for submitting items and checking them off

--- script.js
+++ script.js
@@ -1,13 +1,31 @@
-toDoArr = [];
-doneArr = [];
+done = [];
 
+
+//everything starts with user input
 function addItem () {
-  console.log('adding item')
-  var myInput = document.getElementById('myInput').value,
-  listNode = document.getElementById('todolist'),
-  liNode = document.createElement('LI'),
-  txtNode = document.createTextNode(myInput);
 
-  liNode.appendChild(txtNode);
-  listNode.appendChild(liNode);
-}
+//defining some variables
+  const toDoList = document.getElementById('todolist');
+  const doneList = document.getElementById('done');
+  let myInput = document.getElementById('myInput').value;
+  let newListItem = document.createElement('li');
+
+//add the input to the To Do list
+  newListItem.class = 'newtodo';
+  newListItem.innerHTML = myInput;
+  toDoList.appendChild(newListItem);
+
+//completing  a todo item
+  newListItem.onclick = () => {
+//remove from todo list and add to done list
+    document.getElementById('completed').className = 'done-card show';
+    toDoList.removeChild(newListItem);
+    let newDoneItem = document.createElement('li');
+    newDoneItem.class = 'newdoneitem';
+    newDoneItem.innerHTML = newListItem.innerHTML;
+    doneList.appendChild(newDoneItem);
+    done.push(newDoneItem);
+   };
+
+
+};
